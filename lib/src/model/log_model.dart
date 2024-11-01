0a449aa1b1421b517fc7516b3c2c589c455300f3
Mustafa Türkmen <mustafahussein292@yahoo.com>
2024-11-01 11:35:41 +0300

    feat

--- lib/src/model/log_model.dart
+++ lib/src/model/log_model.dart
@@ -6,20 +6,26 @@ import '../cons/enum.dart';
 class LogModel<T extends IBaseModel> extends IBaseModel<LogModel> {
   LogModel({
     required this.id,
+    this.logTag,
     this.data,
     this.mapData,
   });
 
   LogType id;
+  String? logTag;
   T? data;
   Map<String, dynamic>? mapData;
 
   @override
-  Map<String, dynamic> toJson() => {
-        "type": AppLogger.instance.appId,
-        "id": id.name,
-        "data": [mapData ?? data?.toJson()],
-      };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic>? _data = mapData ?? data?.toJson() ?? {};
+    _data!['logTag'] = logTag;
+    return {
+      "type": AppLogger.instance.appId,
+      "id": id.name,
+      "data": [_data],
+    };
+  }
 
   @override
   LogModel fromJson(Map<String, dynamic> json) {
